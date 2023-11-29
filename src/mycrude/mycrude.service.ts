b7d3171f4d919952de45dd82b7597815106bda8e
Vetrivelmaran <vetri4723@gmai.com>
2023-11-29 19:29:44 +0530

    after change

--- src/mycrude/mycrude.service.ts
+++ src/mycrude/mycrude.service.ts
@@ -1,6 +1,6 @@
-import { Injectable } from "@nestjs/common";
-// import { mainDTO } from "./dto/create-mycrude.dto";
-// import { UpdateMycrudeDto } from "./dto/update-mycrude.dto";
+import { Injectable, NotFoundException } from "@nestjs/common";
+import { mainDTO } from "./dto/create-mycrude.dto";
+import { UpdateMycrudeDto } from "./dto/update-mycrude.dto";
 import { Main, Hero, Heroine } from "./entities/mycrude.entity";
 import { Repository } from "typeorm";
 import { InjectRepository } from "@nestjs/typeorm";
@@ -20,33 +20,67 @@ export class MycrudeService {
 
 
   // get data by id
-  async getbyId(id: number): Promise<Main> {
-    const main = await this.mainRepository
-      .createQueryBuilder('main')
-      .where('main.id = :id', { id })
+  async getbyId(id: number) {
+    const d = await this.mainRepository
+      .createQueryBuilder("main")
+      .where("main.id = :id", { id })
       .getOne();
-      return main;
+    return d;
 
   }    
 
   // get all data from database both master table
-  async getAll(): Promise<{
-    main: Main[];
-  }> {
+  async getAll(){
     const mainData = await this.mainRepository
       .createQueryBuilder("main")
       .getMany();
 
-    return { main: mainData };
+    return mainData;
   }
   
 
-  async getMasterData(): Promise<{ heroes: { id: number; name: string }[]; heroines: { id: number; name: string }[] }> {
-    const heroes = await this.heroRepository.find({ select: ['id', 'name'] });
-    const heroines = await this.heroineRepository.find({ select: ['id', 'name'] });
+  async getMasterData(){
+    const heroes = await this.heroRepository.find({ select: ["id", "name"] });
+    const heroines = await this.heroineRepository.find({
+      select: ["id", "name"],
+    });
 
     return { heroes, heroines };
   }
+
+
+  // update by id
+
+  
+
+  // async updateById(id: number, updateMycrudeDto: UpdateMycrudeDto): Promise<Main> {
+  //   try {
+  //     const existingData = await this.mainRepository.findOne(id);
+
+  //     if (!existingData) {
+  //       throw new NotFoundException(`Data with ID ${id} not found`);
+  //     }
+
+  //     existingData.mName = updateMycrudeDto.mName;
+  //     existingData.heroId = updateMycrudeDto.heroId;
+  //     existingData.heroineId = updateMycrudeDto.heroineId;
+  //     existingData.rDate = updateMycrudeDto.rDate;
+  //     existingData.collection = updateMycrudeDto.collection;
+  //     existingData.rating = updateMycrudeDto.rating;
+
+  //     return await this.mainRepository.save(existingData);
+  //   } catch (error) {
+  //     throw new Error(`Error updating data with ID ${id}: ${error.message}`);
+  //   }
+  // }
+
+
+
+
+
+
+
+
  
   // create(MainDTO: mainDTO) {
   //   return 'This action adds a new mycrude';
